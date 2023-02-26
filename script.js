66791c8710ee5427e806545fd91e25ccfd90e56f
Sarthak248 <88195312+Sarthak248@users.noreply.github.com>
2023-02-25 23:05:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -153,17 +153,34 @@
 
 // *************GOOGLE
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  // $("#name").text(profile.getName());  
-  $(".maintext").css("display","block");
-  $(".g_id_signin").css("display","none");
-  // console.log(googleUser);
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   // $("#name").text(profile.getName());  
+//   $(".maintext").css("display","block");
+//   $(".g_id_signin").css("display","none");
+//   // console.log(googleUser);
+//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//   console.log('Name: ' + profile.getName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
+// }
+
+
+function handleCredentialResponse(response) {
+ // decodeJwtResponse() is a custom function defined by you
+ // to decode the credential response.
+ const responsePayload = decodeJwtResponse(response.credential);
+
+ console.log("ID: " + responsePayload.sub);
+ console.log('Full Name: ' + responsePayload.name);
+ console.log('Given Name: ' + responsePayload.given_name);
+ console.log('Family Name: ' + responsePayload.family_name);
+ console.log("Image URL: " + responsePayload.picture);
+ console.log("Email: " + responsePayload.email);
+
+ $(".maintext").css("display","block");
+ $(".g_id_signin").css("display","none");
 }
 
 function onLoad() {
